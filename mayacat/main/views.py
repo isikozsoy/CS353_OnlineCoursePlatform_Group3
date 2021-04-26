b5b5491f5c7204a8b5f680a116a82b142d09c4c1
melikeaydogan <melike.aydo99@gmail.com>
2021-04-26 22:04:22 +0300

    view changes

--- mayacat/main/views.py
+++ mayacat/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.views.generic import ListView, DetailView, View
-from .models import Course, Lecture
+from .models import Course, Lecture, Announcement, Takes_note, Post, Quest_answ
 
 class CourseListView(ListView):
     model = Course
@@ -17,13 +17,35 @@ class LectureView(View):
         course_queue = Course.objects.filter(slug=course_slug)
         if course_queue.exists():
             course = course_queue.first()
+            print(course)
 
         lecture_q = Lecture.objects.filter(lecture_slug=lecture_slug)
         if lecture_q.exists():
             lecture = lecture_q.first()
 
+        lectures = Lecture.objects.filter(cno_id=course.cno)
+
+        announcements = Announcement.objects.filter( cno_id = course.cno )
+
+        notes = Takes_note.objects.filter( lecture_no_id = lecture.lecture_no ) #student will be added
+
+        lecturecnt = lectures.count()
+
+       # questions = Post.objects.raw('''SELECT postno
+       #                                 FROM Post
+       #                                 WHERE postno NOT IN (SELECT answer_no_id AS postno FROM Quest_answ ); ''')
+
+        #qanda = Quest_answ.objects.all()
+
+
         context = {
-            'object': lecture
+            'curlecture': lecture,
+            'course' : course,
+            'lectures' : lectures,
+            'announcements' : announcements,
+            'notes' : notes,
+            'lecturecnt' : lecturecnt
+            #'questions' : questions
         }
 
         return render(request, 'main/lecture_detail.html', context)
