78a977f0e5637cb6215330d46555f900915bbbde
Sebnem Uslu <48313675+IvoryMask@users.noreply.github.com>
2021-04-29 22:54:47 +0300

    Changes [1]

--- mayacat/main/urls.py
+++ mayacat/main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import CourseListView, CourseDetailView, LectureView, course_detail
+from .views import CourseListView, CourseDetailView, LectureView, Cart_View, Course_View
 
 app_name = 'courses'
 
